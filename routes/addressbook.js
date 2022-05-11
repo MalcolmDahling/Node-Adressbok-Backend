a5ab47c5a3875e9ffc0e70c52d01cf51649a6b27
MalcolmDahling <mac.dahling@gmail.com>
2022-05-11 15:24:22 +0200

    done

---
+++ routes/addressbook.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+
+const cors = require('cors');
+
+router.use(cors());
+
+let fs = require('fs');
+
+
+router.post('/', function(req, res, next) {
+  
+    fs.readFile('addressbook.json', (err, data) => {
+
+        if(err){
+            console.log(err);
+        }
+
+        let addressbook = JSON.parse(data);
+
+        addressbook.push(req.body);
+
+        fs.writeFile("addressbook.json", JSON.stringify(addressbook, null, 4), (err) => {
+            if(err){
+                console.log(err);
+            }
+        });
+
+    });
+});
+
+
+router.get('/', function(req, res){
+
+    fs.readFile('addressbook.json', (err, data) => {
+
+        if(err){
+            console.log(err);
+        }
+        
+        res.send(data);
+    });
+
+});
+
+
+module.exports = router;
+
